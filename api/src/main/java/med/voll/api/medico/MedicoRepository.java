7345677974144a927d384fb272a9d0afd80a10a1
luizfeliperibs <lipezerafar@gmail.com>
2025-10-05 18:00:17 -0300

    feat: mapeia as entidades Medico e Endereco, e controi o repository da entidade Medico com JPA

---
+++ api/src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,10 @@
+package med.voll.api.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Se passa : o nome da entidade "," tipo do atribto da chave primária da entidade
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+
+
+
+}
